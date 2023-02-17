89274defa4ddde2024b61edefdbb8719b8c7605b
Nwamaka Okolocha <eamaka@gmail.com>
2023-02-17 02:44:42 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main -Entry
- * Return: Always 0
+ * main - print a line of code using printf
+ * Return: 0
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
