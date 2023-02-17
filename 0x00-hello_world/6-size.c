461aaacdee30ec9811f2944bb1c9e063d44ed5d8
Nwamaka Okolocha <eamaka@gmail.com>
2023-02-17 03:35:17 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,6 +4,13 @@
  * Return: Always 0
  */
 int main(void)
+{
+
+	char;
+	int;
+	long int;
+	long long int;
+	float;
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of a int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
@@ -12,5 +19,5 @@ int main(void)
 	return (0);
 
 
-
+}
 
