6a2a185447ccf00e1b16eafe78f8ca3345b9bc93
Nwamaka Okolocha <eamaka@gmail.com>
2023-02-17 02:30:02 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/**
- * main - Entry
- * Return: Always 0
+/*
+ * main - print a line of code using puts
+ * Return: 0
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multi lingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
